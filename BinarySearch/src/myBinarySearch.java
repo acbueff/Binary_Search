94194a3a24bf6bc6020612254aeea824ae80e60c
Andreas <acbueff@gmail.com>
2014-01-31 10:39:03 -0800

    created second binaryfile; myBinaryFile; does not include empty constructor

--- BinarySearch/src/myBinarySearch.java
+++ BinarySearch/src/myBinarySearch.java
@@ -8,15 +8,16 @@ import java.util.Arrays;
  */
 public class myBinarySearch {
 	/**
-	 * 
-	 * @param key
-	 * @param a
-	 * @return
+	 * Search for int key within sorted int array a[]
+	 * @param key the int to be searched
+	 * @param a array of integers, must be sorted in ascending order
+	 * @return index of key in array a[ if present; -1 if not present
 	 */
 	public static int rank(int key, int[] a){
 		int lo = 0;
 		int hi = a.length - 1;
 		while(lo <= hi){
+			//Key is in a[lo..hi or not present
 			int mid = lo + (hi - lo)/2;
 			if(key < a[mid] ){
 				hi = mid - 1;
@@ -29,5 +30,31 @@ public class myBinarySearch {
 		}
 		return -1;
 	}
-
+	
+	/**
+	 * Reads in a sequence of ints from the whitelist file, specifies as command line 
+	 * argument. Reads in ints from standard input and prints to standard output those ints
+	 * that do not appear in file
+	 * @param args
+	 */
+	
+	public static void main(String[] args){
+		
+		//read the integers from a file
+		In in = new In(args[0]);
+		int[] whitelist = In.readInts(args[0]);
+		
+		//sort the array
+		Arrays.sort(whitelist);
+		
+		//read integer key from standard input; print if not in whitelist
+		while(!StdIn.isEmpty())
+		{
+			int key = StdIn.readInt();
+			if (rank(key, whitelist) == -1)
+				StdOut.println(key);
+		}
+	}
+		
 }
+		
